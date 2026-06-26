15403d27ddf23c50bf92ad3184a367af93106174
Milansuman <milansuman2016@gmail.com>
2026-06-26 09:26:18 +0530

    feat: Add document table migrations

--- alembic/env.py
+++ alembic/env.py
@@ -11,6 +11,7 @@ from env import env
 import models.employee  # noqa: F401
 import models.address  # noqa: F401
 import models.department  # noqa: F401
+import models.document  # noqa: F401
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
